73bf050478c497a689d0fb206059e81ea54ca667
sallyhalal <sally.halal@yahoo.com>
2016-04-17 20:52:20 +0300

    Integration of googleMaps in ApplicationForm we still need to guard the GPSenable The googleMaps shown shows the direction from the user's current location to a fixed location we have to change only that ( destPosition as LantLang format) for every different location which can be sent as intent.

--- app/src/main/java/com/media/elte/elte_ckeckin/ApplicationForm.java
+++ app/src/main/java/com/media/elte/elte_ckeckin/ApplicationForm.java
@@ -1,13 +1,40 @@
 package com.media.elte.elte_ckeckin;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+import android.widget.TextView;
 
-public class ApplicationForm extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.w3c.dom.Document;
+
+import java.util.ArrayList;
+
+public class ApplicationForm extends AppCompatActivity implements LocationListener {
+
+    LocationManager mLocationManager;
+    private TextView tvInfo;
+
+    private GoogleMap map;
+    MapView mMapView;
+    LatLng sourcePosition, destPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +43,117 @@ public class ApplicationForm extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
+
+        mMapView = (MapView) findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();// needed to get the map to display immediately
+        try {
+            MapsInitializer.initialize(getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        initiateMap();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location != null ) {
+            // Do something with the recent location fix
+            //  otherwise wait for the update below
+            tvInfo.setText("Location !!! Changed"+location.getLatitude() + " and " + location.getLongitude() +"  AT" + location.getTime());
+        }
+        else {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+        }
+        sourcePosition= new LatLng(location.getLatitude(),location.getLongitude());
+        destPosition = new LatLng( 47.499425, 19.055139);
+        route(sourcePosition,destPosition);
     }
 
+    protected void route(LatLng sourcePosition, LatLng destPosition) {
+
+        final Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
+                try {
+                    Document doc = (Document) msg.obj;
+                    GMapV2Direction md = new GMapV2Direction();
+                    ArrayList<LatLng> directionPoint = md.getDirection(doc);
+                    PolylineOptions rectLine = new PolylineOptions().width(10).color(Color.BLUE);
+
+                    for (int i = 0; i < directionPoint.size(); i++) {
+                        rectLine.add(directionPoint.get(i));
+
+                    }
+
+                    map.addPolyline(rectLine);
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(rectLine.getPoints().get(0), 15.0f));
+
+
+                    tvInfo.setText("DURATION" + md.getDurationText(doc));
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            ;
+        };
+
+        new GMapV2DirectionAsyncTask(handler, sourcePosition, destPosition, GMapV2Direction.MODE_WALKING).execute();
+    }
+
+    private void initiateMap() {
+        map = mMapView.getMap();
+        map.setMyLocationEnabled(true);
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            // tvInfo.setText("Location Changed"+location.getLatitude() + " and " + location.getLongitude() +"  AT" + location.getTime());
+            Log.d("Location Changed", location.getLatitude() + " and " + location.getLongitude());
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mLocationManager.removeUpdates(this);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
--- app/src/main/java/com/media/elte/elte_ckeckin/MainMenu.java
+++ app/src/main/java/com/media/elte/elte_ckeckin/MainMenu.java
@@ -59,6 +59,7 @@ public class MainMenu extends AppCompatActivity {
                 break;
             case R.id.btnLifeStyle:
                 i.setClass(this,LifeStyleMenu.class);
+
                 startActivity(i);
                 break;
             default:
